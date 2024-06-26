70542e633eddbb0e5ab58ceb7a031404fd803d4e
Brunna <brunna.bonilha@genstudents.org>
2024-06-26 13:52:54 -0300

    Exercicios javascript

---
+++ operadores/IncreDesc.js
@@ -0,0 +1,41 @@
+//-----------------------Pré Incrementar---------------------//
+
+let x = 10;
+let y = 8;
+
+console.log("\nPré Incrementar: x = ++ y");
+
+x = ++y;
+
+console.log("\nValor de X:" + x);
+console.log("\nValor de Y:" + y);
+
+//Ele resolve o y primeiro adicionando um numero e depois atribui ao x//
+
+//------------------Pré Decrementar-----------------//
+
+let x = 10;
+let y = 8;
+
+console.log("\nPré Decrementar: x = --y");
+
+x = --y;
+
+console.log("\nValor de X:" + x);
+console.log("\nValor de Y:" + y);
+
+//Ele resolve o valor de y subtraindo um numero e depois atribui ao x//
+
+ //-------------------- Pós Incrementar--------------//
+
+ let i = 40;
+ let j = 30;
+
+ console.log("\nPós Incrementar: i = j ++");
+
+ i = j++
+
+ console.log("\nValor de I:" + i);
+console.log("\nValor de J:" + j);
+
+//Ele resolve prmeiro i e depois acrescenta um numero no j
---
+++ variaveis/variaveisConstante.js
@@ -0,0 +1,14 @@
+// let numero = 4;
+
+// numero = "false";
+
+// console.log(numero)
+
+var identificador_da_variavel = 1; //  funciona global, no cógido todo //
+let indentificador_da_variavel2 = "1"; // funciona dentro de um escopo {} //
+
+if(identificador_da_variavel1 == 1){
+     var identificador_da_variavel = 1;
+
+console.log(indentificador_da_variavel2)
+}
